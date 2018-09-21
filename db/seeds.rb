97762091028a5d4b7bf77dcd79ce47e495a1ba9c
learn <learn@learn02.localdomain>
2018-09-20 17:07:32 -0700

    Properly linked models, added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(first_name:'Jerome', last_name:'Sheppard', email:'jerome@email.com', password:'password1')
+
+user1 = User.create(first_name:'Mary', last_name:'San Agustin', email:'mary@email.com', password:'password2')
+
+user2 = User.create(first_name:'Enrique', last_name:'Vega', email:'enrique@email.com', password:'password3')
+
+Apartment.create(user_id: user1.id, street1:'123 J st', street2:'Apt 1', city:'San Diego', postal_code:92116, state:'CA', country:'United States', manager_name:'Jez Go', phone_number:'123-454-6789', contact_hours:'M-F: 9-5')
+
+Apartment.create(user_id: user1.id, street1:'456 K st', street2:'Apt 2', city:'San Diego', postal_code:92105, state:'CA', country:'United States', manager_name:'Jez Go', phone_number:'234567890', contact_hours:'M-F: 9-5')
+
+Apartment.create(user_id: user2.id, street1:'789 L st', street2:'Apt 3', city:'San Diego', postal_code:92568, state:'CA', country:'United States', manager_name:'Jez Go', phone_number:'345678901', contact_hours:'M-F: 9-5')
